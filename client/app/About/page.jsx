f7818dcd94038d2381e5d35dc77f8a8b4661e54e
93amrkakashi <93amr.kakashi@gmail.com>
2023-11-27 11:23:47 +0200

    theme

--- client/app/About/page.jsx
+++ client/app/About/page.jsx
@@ -10,7 +10,7 @@ const About = () => {
   return (
     <main className={`  `}>
       <div className="flex flex-col items-center justify-around">
-        <div dir="ltr" className={`hero flex justify-center items-center pt-12 min-h-[95vh] ${theme === "dark" ?  "bg-slate-900": "bg-indigo-700"} w-full`} >
+        <div dir="ltr" className={`hero flex justify-center items-center pt-12 min-h-[95vh] bg-indigo-700 w-full`} >
           <div className="flex flex-col justify-center gap-5 items-center">
             <h1 className="font-bold text-6xl text-gray-50 ">Scripto Code</h1>
             <p className="font-bold text-5lg text-gray-100  ">Your Programming Solutions Partner</p>
