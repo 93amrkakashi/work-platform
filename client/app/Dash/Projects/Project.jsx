084beef21c513398118c660f10fd067ad15fb574
93amrkakashi <93amr.kakashi@gmail.com>
2023-11-27 18:56:01 +0200

    projects

--- client/app/Dash/Projects/Project.jsx
+++ client/app/Dash/Projects/Project.jsx
@@ -3,14 +3,13 @@ import React from 'react'
 
 const Project = ({project}) => {
   return (
-    <div className='w-[300px] md:w-[400px] border rounded-lg flex flex-col items-center justify-center '>
-      <div className="img w-full h-[250px] rounded-lg">
-      <img src={project.image} />
-      </div>
-      <p>{project.name}</p>
-      <p>{project.description}</p>
-      <p>{project.category}</p>
-    </div>
+    <div dir='rtl' className='img-con w-[300px] md:w-[400px] border rounded-lg flex flex-col items-center justify-center hover:transform-hover'>
+  <div className="img w-full h-[250px] rounded-lg">
+    <img src={project.image} />
+  </div>
+  <p className='text-xl font-bold'>{project.name}</p>
+  <p className='text-center p-2 min-h-[150px]'>{`${project.description.slice(0, 250)}....`}</p>
+</div>
   )
 }
 
